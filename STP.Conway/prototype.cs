0224b03c4a1ebe999d3d7533ecb36b45d565a14d
Shaun Padachi <shaun.padachi@gmail.com>
2017-12-13 23:57:23 +0200

    Added Comments and refactored.

--- STP.Conway/prototype.cs
+++ STP.Conway/prototype.cs
@@ -8,70 +8,67 @@ namespace STP.Conway
 {
   class Prototype  // used for jotting down idea/concept
   {
-    List<List<int>> _field;
+    // we assume the field can be any size
+    // alive = 1
+    List<List<int>> _grid;
 
-    public Prototype(List<List<int>> field)  // alive = 1
+    public Prototype(List<List<int>> grid)
     {
-      _field = field;
+      _grid = grid;
     }
 
 
-    public void DoSomething()
+    // This represents ONE GENERATION
+    public void ApplyRules()
     {
       int r = -1;
       int c = -1;
-      foreach (var row in _field)
+      foreach (var row in _grid)
       {
         r++;  //counter for row - can be done better
         foreach (var element in row)
         {
           c++;  //counter for column - can be done better
-          Kill(r, c, element);
-          BringToLife(r, c, element);
+          if (element == 1) Kill(r, c);
+          else
+          if (element == 0) BringToLife(r, c);
         }
         c = -1;
       }
     }
 
-    private void BringToLife(int r, int c, int element)
+    private void BringToLife(int r, int c)
     {
-      int aliveCells = 0;
-      if (element == 0)  // if current cell is dead
-      {
-        aliveCells = GetCountOfSurroundingLiveCells(r, c);
-        _field[r][c] = aliveCells == 3 ? _field[r][c] = 1 : _field[r][c] = 0;
-      }
+      var aliveCells = GetCountOfSurroundingLiveCells(r, c);
+      _grid[r][c] = aliveCells == 3 ? _grid[r][c] = 1 : _grid[r][c] = 0;
     }
 
-    private void Kill(int r, int c, int element)
+    private void Kill(int r, int c)
     {
-      int aliveCells = 0;
-      if (element == 1)  // if current cell is alive
-      {
-        aliveCells = GetCountOfSurroundingLiveCells(r, c);
-        _field[r][c] = aliveCells == 3 || aliveCells == 2 ? _field[r][c] = 1 : _field[r][c] = 0;
-      }
+      var aliveCells = GetCountOfSurroundingLiveCells(r, c);
+      _grid[r][c] = aliveCells == 3 || aliveCells == 2 ? _grid[r][c] = 1 : _grid[r][c] = 0;
     }
 
-    private int GetCountOfSurroundingLiveCells( int r, int c)
+    //probably could use a better name
+    private int GetCountOfSurroundingLiveCells(int r, int c)
     {
-      int count = 0; 
+      int count = 0;
 
       if (r != 0)
       {
-        if (c != 0 )                 count = count + _field[r - 1][c - 1];
-                                     count = count + _field[r - 1][c];
-        if (c != _field[0].Count-1 ) count = count + _field[r - 1][c + 1];
+        if (c != 0) count = count + _grid[r - 1][c - 1];
+        count = count + _grid[r - 1][c];
+        if (c != _grid[0].Count - 1) count = count + _grid[r - 1][c + 1];
       }
 
-      if (c != 0)                     count = count + _field[r][c - 1];
-      if (c != _field[0].Count - 1)   count = count + _field[r][c + 1];
+      if (c != 0) count = count + _grid[r][c - 1];
+      if (c != _grid[0].Count - 1) count = count + _grid[r][c + 1];
 
-      if (r != _field.Count - 1)
+      if (r != _grid.Count - 1)
       {
-       if (c != 0 )                   count = count + _field[r + 1][c - 1];
-                                      count = count + _field[r + 1][c];
-        if (c != _field[0].Count - 1) count = count + _field[r + 1][c + 1];
+        if (c != 0) count = count + _grid[r + 1][c - 1];
+        count = count + _grid[r + 1][c];
+        if (c != _grid[0].Count - 1) count = count + _grid[r + 1][c + 1];
       }
       return count;
     }
